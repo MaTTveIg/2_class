f52b6db830c5aab379633540c02a68b56cbb8dc9
Matvey Vizir <112975341+MaTTveIg@users.noreply.github.com>
2023-05-21 17:21:51 +0200

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_class
+{
+    internal class Figure
+    {
+        private Point sides;
+        public Figure(in int sideA, in int sideB, in int sideC)
+        {
+            sides.sideA = sideA;
+            sides.sideB = sideB;
+            sides.sideC = sideC;
+        }
+
+        public Point GetSides() => sides;
+        public int FindPerimetr() => sides.sideA + sides.sideB + sides.sideC;
+        public int FindSquare()
+        {
+            int halphPerimetr;
+            halphPerimetr = (sides.sideA + sides.sideB + sides.sideC) / 2;
+
+            int s = halphPerimetr * (halphPerimetr - sides.sideA) *
+                (halphPerimetr - sides.sideB) * (halphPerimetr - sides.sideC);
+
+            return Convert.ToInt32(Math.Sqrt(s));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using _2_class;
+
+Figure triangle = new(3, 5, 6);
+
+Console.WriteLine(triangle.FindSquare());
+
+struct Point
+{ 
+    public int sideA;
+    public int sideB;
+    public int sideC;
+};
